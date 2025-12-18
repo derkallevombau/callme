e6f9b506d09812bc3403449402b850824045c80c
derkallevombau <noreply>
2025-12-18 07:28:32 +0100

    Keep console window open if we created our own.
    Change Subsystem from Console to Windows and modify name and
    signature of `main` accordingly to start without being connected to any
    console.
    Then, try to attach to console of parent process. If that fails, we know
    that we have to keep the window open.

--- callme/callme.cpp
+++ callme/callme.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <format>
-
-#ifdef _WIN32
 #include <windows.h>
+#include <sal.h>
+#include <string>
+#include <cstdio>
 
-// Added by Copilot to enable processing of ANSI escape sequences.
-// dwMode is 7 when starting in WT (thus VT is already enabled),
-// but it cannot hurt to keep this.
+#include "colordefs.h"
+
+// Enables processing of ANSI escape sequences.
+// When starting in WT, dwMode is 7 (ENABLE_VIRTUAL_TERMINAL_PROCESSING == 4,
+// thus VT processing is already enabled), but after AllocConsole(), it is 3,
+// so we need this.
 // N.B.: A static *function* can only be called from within the same
 // translation unit, which simplifies linkage.
 static void enableVirtualTerminalProcessing()
@@ -20,41 +24,90 @@ static void enableVirtualTerminalProcessing()
 	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 	SetConsoleMode(hOut, dwMode);
 }
-#endif
 
-constexpr auto ansiBlack = "\x1b[30m";
-constexpr auto ansiRed = "\x1b[31m";
-constexpr auto ansiGreen = "\x1b[32m";
-constexpr auto ansiYellow = "\x1b[33m";
-constexpr auto ansiBlue = "\x1b[34m";
-constexpr auto ansiMagenta = "\x1b[35m";
-constexpr auto ansiCyan = "\x1b[36m";
-constexpr auto ansiWhite = "\x1b[37m";
-
-constexpr auto ansiLightBlack = "\x1b[90m";
-constexpr auto ansiLightRed = "\x1b[91m";
-constexpr auto ansiLightGreen = "\x1b[92m";
-constexpr auto ansiLightYellow = "\x1b[93m";
-constexpr auto ansiLightBlue = "\x1b[94m";
-constexpr auto ansiLightMagenta = "\x1b[95m";
-constexpr auto ansiLightCyan = "\x1b[96m";
-constexpr auto ansiLightWhite = "\x1b[97m";
-
-constexpr auto ansiReset = "\x1b[0m";
-
-int main(int argc, char* argv[])
+// Entry point when linking with /SUBSYSTEM:WINDOWS (Unicode version), as opposed to
+// `main` when linking with /SUBSYSTEM:CONSOLE.
+// See https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
+// and https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain.
+// Annotations: See https://stackoverflow.com/questions/13078953/code-analysis-says-inconsistent-annotation-for-wwinmain-this-instance-has-no.
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
-#ifdef _WIN32
+	// See https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-commandlinetoargvw.
+	int argc;
+	LPWSTR* argv = CommandLineToArgvW(pCmdLine, &argc);
+
+	// Try to attach to parent process console first (if started from terminal) 
+	// See https://learn.microsoft.com/en-us/windows/console/attachconsole
+	bool attachedToParent = AttachConsole(ATTACH_PARENT_PROCESS);
+
+	// If not started from console, allocate a new one
+	if (!attachedToParent)
+	{
+		// See https://learn.microsoft.com/en-us/windows/console/allocconsole.
+		if (!AllocConsole())
+		{
+			LocalFree(argv);
+			return -1;
+		}
+	}
+
+	// Redirect standard streams to the console (whether attached or allocated).
+	// This is necessary because this is not a console application, which in turn
+	// is necessary to be able to detect if we have been started from within a console
+	// or not.
+	// freopen_s closes the file associated with the file stream pointed to by the
+	// fourth argument (FILE*), then opens the file specified by the second arg and
+	// associates that file with the file stream pointed to by the fourth arg.
+	// The pointer whose address has been passed in the first arg is set to point to
+	// that file stream too; we use dummies here.
+	// The crucial point is that an existing file stream (here the standard streams)
+	// is associated with a newly opened file.
+	// See https://en.cppreference.com/w/c/io/freopen.
+	FILE* pFileIn = nullptr;
+	FILE* pFileOut = nullptr;
+	FILE* pFileErr = nullptr;
+	freopen_s(&pFileIn, "CONIN$", "r", stdin);
+	freopen_s(&pFileOut, "CONOUT$", "w", stdout);
+	freopen_s(&pFileErr, "CONOUT$", "w", stderr);
+
+	// Synchronize C++ streams with C streams
+	std::cin.clear();
+	std::cout.clear();
+	std::cerr.clear();
+
 	enableVirtualTerminalProcessing();
-#endif
 
 	for (int i = 0; i < argc; i++)
 	{
+		// Convert wide string to narrow string for std::format
+		std::wstring warg(argv[i]);
+		std::string arg(warg.begin(), warg.end());
+
 		// N.B.: Using "\n" instead of std::endl saves unnecessary flushes.
 		// N.B. 2: It's "\n" also on Windows; the runtime choses the correct
 		// line break for the platform.
 		// N.B. 3: std::format is a C++20 feature.
 		std::cout << std::format("{}{}{}: >{}{}{}<\n", ansiLightGreen, i, ansiReset,
-			i % 2 ? ansiLightCyan : ansiLightMagenta, argv[i], ansiReset);
+			i % 2 ? ansiLightCyan : ansiLightMagenta, arg, ansiReset);
 	}
+
+	if (!attachedToParent)
+	{
+		// Wait for user input before exiting since we are running in our own
+		// console window which is closed immediately on exit.
+		std::cout << "\nPress any key to exit.";
+
+		char* pInput = new char[1];
+		std::cin.read(pInput, 1);
+		delete[] pInput;
+
+		// Free allocated console (but not an existing one to which we attached).
+		FreeConsole();
+	}
+
+	// Free memory allocated by CommandLineToArgvW.
+	// See https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-commandlinetoargvw
+	LocalFree(argv);
+
+	return 0;
 }
